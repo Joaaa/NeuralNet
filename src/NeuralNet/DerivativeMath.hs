aab505569071726d391191498f371a5768bd69c6
Joa <joa.oris@hotmail.com>
2020-04-19 10:08:16 +0200

    Some random stuff

--- src/NeuralNet/DerivativeMath.hs
+++ src/NeuralNet/DerivativeMath.hs
@@ -4,52 +4,38 @@ module NeuralNet.DerivativeMath where
 import Data.Data
 import Control.Lens
 
-data Calculation = DSum Calculation Calculation
-  | DProd Calculation Calculation
-  | DAbs Calculation
-  | DSignum Calculation
-  | DConst Float
-  | DVar String
+data Calculation = CSum Calculation Calculation
+  | CProd Calculation Calculation
+  | CAbs Calculation
+  | CSignum Calculation
+  | CVar String
+  | CConst Float
   deriving (Typeable, Data, Eq)
 
 instance Plated Calculation
 
-cSum (DConst 0) r = r
-cSum l (DConst 0) = l
-cSum (DConst l) (DConst r) = DConst $ l + r
-cSum l r = DSum l r
-
-cProd (DConst 0) r = DConst 0
-cProd l (DConst 0) = DConst 0
-cProd (DConst 1) r = r
-cProd l (DConst 1) = l
-cProd (DConst l) (DConst r) = DConst $ l * r
-cProd (DSum sl sr) r = cSum (cProd sl r) (cProd sr r)
-cProd l (DSum sl sr) = cSum (cProd l sl) (cProd l sr)
-cProd l r = DProd l r
-
 instance Num Calculation where
-  l + r = DSum l r
-  l * r = DProd l r
-  abs = DAbs
-  signum = DSignum
-  fromInteger = DConst . fromInteger
-  negate a = DProd a (fromInteger (-1))
+  l + r = CSum l r
+  l * r = CProd l r
+  abs = CAbs
+  signum = CSignum
+  fromInteger = CConst . fromInteger
+  negate a = CProd a (fromInteger (-1))
 
 deriveTo :: Calculation -> Calculation -> Calculation
-deriveTo x (DSum l r) = cSum (deriveTo x l) (deriveTo x r)
-deriveTo x (DProd l r) = cSum (cProd (deriveTo x l) r) (cProd l (deriveTo x r))
-deriveTo x (DAbs a) = cProd (DSignum a) (deriveTo x a)
-deriveTo x (DSignum _) = 0
-deriveTo x (DConst _) = 0
-deriveTo (DVar x) (DVar y)
+deriveTo x (CSum l r) = CSum (deriveTo x l) (deriveTo x r)
+deriveTo x (CProd l r) = CSum (CProd (deriveTo x l) r) (CProd l (deriveTo x r))
+deriveTo x (CAbs a) = CProd (CSignum a) (deriveTo x a)
+deriveTo x (CSignum _) = 0
+deriveTo x (CConst _) = 0
+deriveTo (CVar x) (CVar y)
   | y == x = 1
   | otherwise = 0
 
 instance Show Calculation where
-  show (DSum a b) = show a <> " + " <> show b
-  show (DProd a b) = show a <> " * " <> show b
-  show (DAbs a) = "abs(" <> show a <> ")"
-  show (DSignum a) = "signum(" <> show a <> ")"
-  show (DConst c) = show c
-  show (DVar n) = n
+  show (CSum a b) = "(" <> show a <> " + " <> show b <> ")"
+  show (CProd a b) = "(" <> show a <> " * " <> show b <> ")"
+  show (CAbs a) = "abs(" <> show a <> ")"
+  show (CSignum a) = "signum(" <> show a <> ")"
+  show (CConst c) = show c
+  show (CVar n) = n
--- src/NeuralNet/Test.hs
+++ src/NeuralNet/Test.hs
@@ -3,24 +3,47 @@ module NeuralNet.Test where
 import Data.Monoid (Sum)
 import NeuralNet.DerivativeMath
 import Control.Lens
+import Util
 
 main :: IO ()
 main = do
   putStrLn "Test"
-  let x = DVar "x"
-  let y = DVar "y"
+  let x = CVar "x"
+  let y = CVar "y"
   let expr = 5 * x + 3 * (x * y) + 7 + (5 * x * x * x)
   let der = deriveTo x expr
   print expr
-  print $ expr & plate .~ DConst 8
-  print $ universe expr
-  print $ transform add1 expr
   print $ transform simplify expr
+  putStrLn "---"
   print der
-  print $ transform simplify der
+  print $ nTimes 100 (transform simplify) der
 
-add1 (DConst i) = DConst $ i+1
-add1 a = a
 
-simplify (DSum l r) | l == r = cProd 2 l
+simplify :: Calculation -> Calculation
+simplify (CSum l r) | shouldSwitch l r = CSum r l
+simplify (CProd l r) | shouldSwitch l r = CProd r l
+
+simplify (CSum (CConst l) (CConst r)) = CConst (l + r)
+simplify (CProd (CConst l) (CConst r)) = CConst (l * r)
+
+simplify (CSum (CConst 0) r) = r
+simplify (CProd (CConst 0) r) = CConst 0
+simplify (CProd (CConst 1) r) = r
+
+simplify (CSum (CSum ll lr) r) = ll + (lr + r)
+simplify (CProd (CProd ll lr) r) = ll * (lr * r)
+
+simplify (CSum (CProd (CConst l1) r1) (CProd (CConst l2) r2)) | r1 == r2 = (r1 + r2) * r1
+simplify (CSum r1 (CProd (CConst l2) r2)) | r1 == r2 = (1 + r2) * r1
+simplify (CSum (CProd (CConst l1) r1) r2) | r1 == r2 = (r1 + 1) * r1
+
+simplify (CProd (CSum ll lr) r) = ll * r + lr * r
+simplify (CProd l (CSum rl rr)) = l * rl + l * rr
+
 simplify a = a
+
+shouldSwitch :: Calculation -> Calculation -> Bool
+shouldSwitch l (CConst _) = case l of
+  (CConst _) -> False
+  _ -> True
+shouldSwitch _ _ = False
